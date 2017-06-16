42ee489cdad3acb340d2c0718dada1939baf2f59
mcmahonjohn <mcmahonjohn@users.noreply.github.com>
2017-06-15 23:53:14 -0400

    Small steps for plugin development

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
+"use strict";
+
 module.exports = (robot) => {
-  // Your plugin code here
   console.log('Yay, the plugin was loaded!');
-  
+
+  robot.on('create', async context => {
+    var payload = context.payload;
+
+    if ( payload.ref_type === 'branch' ) {
+      robot.log(context);
+    }
+
+  }
+  // If the branch does not meet naming standard, then create an issue listing that branch
+  // Track any issues created and whether they get closed/completed. If so, create a new one.
+
   // For more information on building plugins:
   // https://github.com/probot/probot/blob/master/docs/plugins.md
-  
+
   // To get your plugin running against GitHub, see:
   // https://github.com/probot/probot/blob/master/docs/development.md
-};
+}
