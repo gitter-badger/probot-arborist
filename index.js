2f0a6c46eca9fbe72d8cf72c82338e96e1875ed3
mcmahonjohn <mcmahonjohn@users.noreply.github.com>
2017-06-19 00:35:12 -0400

    Plugin gains ability to catch events when a branch is created

--- index.js
+++ index.js
@@ -1,22 +1,15 @@
 "use strict";
 
-module.exports = (robot) => {
-  console.log('Yay, the plugin was loaded!');
+module.exports = function (robot) {
 
   robot.on('create', async context => {
     var payload = context.payload;
 
     if ( payload.ref_type === 'branch' ) {
-      console.log(context);
+      console.log("caught branch");
     }
 
   })
   // If the branch does not meet naming standard, then create an issue listing that branch
   // Track any issues created and whether they get closed/completed. If so, create a new one.
-
-  // For more information on building plugins:
-  // https://github.com/probot/probot/blob/master/docs/plugins.md
-
-  // To get your plugin running against GitHub, see:
-  // https://github.com/probot/probot/blob/master/docs/development.md
-}
+};
